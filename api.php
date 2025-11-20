e3b279f76d4147968290affa1720f0c9d6198a77
Koncepcja77 <71903474+Koncepcja77@users.noreply.github.com>
2025-11-20 10:17:26 +0100

    Update api.php

--- api.php
+++ api.php
@@ -1,6 +1,18 @@
 <?php
+// --- CORS ---
+header("Access-Control-Allow-Origin: *"); // albo wpisz konkretny adres np. http://192.168.0.23:3000
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Authorization, Content-Type, X-Requested-With");
+
+// Obsługa preflight (OPTIONS)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Origin: *");
+
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
